2ba2185da42bc2e208b8e821d1c1ac031f080587
Marius Gjerde <marius@mmgjerde.no>
2022-02-10 20:16:44 +0100

    test

---
+++ main.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from os import getenv
+import discord
+from discord.ext import commands
+# from discord.ext.commands import context
+
+
+load_dotenv()
+DEBUG_GUILD = list()
+DEBUG_GUILD.append(int(getenv('DEBUG_GUILD')))
+TOKEN = getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.all()
+denbot = commands.Bot(intents=intents, debug_guilds=DEBUG_GUILD)
+denbot.load_extension("moderator_commands")
+denbot.load_extension("seasonal_winter")
+denbot.load_extension("lfg")
+denbot.load_extension("gameshare")
+
+denbot.run(TOKEN) 
