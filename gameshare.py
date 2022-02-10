ebdd065f5e3ec6e1eff9323723e7ede9de0a5aa6
Marius Gjerde <marius@mmgjerde.no>
2022-02-10 20:45:05 +0100

    added files

---
+++ gameshare.py
@@ -0,0 +1,88 @@
+import discord
+
+from discord.ext import commands
+from discord.commands import Option
+from discord import context
+import json
+import urllib.request
+from discord import SlashCommandGroup
+
+
+class GameSelect(discord.ui.Select):
+    def __init__(self):
+
+        super().__init__(
+            placeholder="Search results, select your game",
+            min_values=1,
+            max_values=1,
+            options=[],
+        )
+    async def callback(self, interaction: discord.Interaction):
+
+        await interaction.response.send_message(content=self.values[0])
+
+class GameView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+    
+
+    @discord.ui.button(label="Share", style=discord.ButtonStyle.gray )
+    async def share(self, button: discord.ui.Button, interaction: discord.Interaction):
+        pass
+    
+    @discord.ui.button(label="Donate", style=discord.ButtonStyle.gray, emoji="🎁")
+    async def donate(self, button: discord.ui.Button, interaction: discord.Interaction):
+        pass
+    
+
+
+class GameButton(discord.ui.Button):
+    def __init__(self, buttonname: str, emoji: str):
+        """
+        A button for one role. `custom_id` is needed for persistent views.
+        """
+        super().__init__(
+            label=buttonname,
+            style=discord.enums.ButtonStyle.grey,
+            emoji=emoji
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+
+        pass 
+        
+class Games(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+
+    @commands.slash_command(default_permission=True, name="lookup", description="Donate a game to the community")
+    async def addgame(self, ctx: context,
+                    game: Option(str, "What game do you want to add?")):
+
+        # This will crash if non-basic letters like æøå is being used, library bug or server settings? halp
+        query = game.replace(" ", "%20")
+        url = urllib.request.urlopen(f"https://api.rawg.io/api/games?key=4c63ca18e8234c849fdc082eb88b6925&search={query}&page_size=5")
+        rawglist = json.load(url)
+
+        selectlist = []
+        if rawglist["results"]:
+            gameselector = GameSelect() 
+            searchview = GameView()
+            searchview.add_item(gameselector)
+            
+            for game in rawglist["results"]:
+                gameselector.add_option(label=f"{game['name']}", value = game['id'], emoji="🎮")
+            
+            await ctx.respond("Game lookup", ephemeral=True, view=searchview)
+        else:
+            await ctx.respond("No games found...", ephemeral=True
+            )
+
+
+  
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Games(bot))
+    
---
+++ lfg.py
@@ -0,0 +1,26 @@
+from os import getenv
+from discord.ext.commands import context
+from discord.ext import commands
+from discord.utils import get
+
+LFG_ROLE = getenv('LFG_ROLE')
+
+class Lfg(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_presence_update(self, before, after):
+        lfg_role = get( before.guild.roles, name=LFG_ROLE )
+        if str(before.status) in ("online", "idle", "dnd") and str(after.status) == "offline" and lfg_role in after.roles:
+            await after.remove_roles(lfg_role)
+
+    @commands.slash_command(default_permission=True, name="lfg", description="Looking for peeps to play with")
+    async def lfg(self, ctx: context):  # user commands return the member
+        lfg_role = get( ctx.author.guild.roles, name=LFG_ROLE )
+        await ctx.author.add_roles(lfg_role)
+        await ctx.respond("LFG role given!", ephemeral=True)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Lfg(bot))
