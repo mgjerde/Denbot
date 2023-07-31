9662192e84a7884b0257d20448779b8163cb9c35
Marius Toftelund Gjerde <marius@mgjerde.no>
2023-07-31 23:20:01 +0200

    Add logging and add guild to db on join

--- src/database.py
+++ src/database.py
@@ -1,9 +1,12 @@
 import sqlite3
 class Database:
     
-    def __init__(self, dbloc):
-        self.__connection = sqlite3.connect(dbloc)
+
+    def __init__(self):
+        self.__DB_LOCATION = "/app/data/denbot.sqlite"
+        self.__connection = sqlite3.connect(self.__DB_LOCATION)
         self.cur = self.__connection.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS servers (guild_id NOT NULL PRIMARY KEY, ac_channel, streaming_role, lfg_role, auto_role);")
 
     def __del__(self):
         self.__connection.close()
@@ -18,24 +21,23 @@ class Database:
     def commit(self):
         self.__connection.commit()
 
-__DB_LOCATION = "/app/data/denbot.sqlite"
 
 def create_table():
-    db = Database(__DB_LOCATION)
+    db = Database()
     db.execute("CREATE TABLE IF NOT EXISTS servers (guild_id NOT NULL PRIMARY KEY, ac_channel, streaming_role, lfg_role, auto_role);")
 
 def add_guild(guild):
-    db = Database(__DB_LOCATION)
+    db = Database()
     db.execute(f"INSERT OR IGNORE INTO servers (guild_id) VALUES ({guild});")
     db.commit()
 
 def update_setting(guild_id, custom_id, value):
-    db = Database(__DB_LOCATION)
+    db = Database()
     db.execute(f"UPDATE servers SET {custom_id} = {value} WHERE guild_id LIKE {guild_id};")
     db.commit()
 
 def get_setting(guild_id, setting_name):
-    db = Database(__DB_LOCATION)
+    db = Database()
     return db.execute(f"SELECT {setting_name} FROM servers WHERE guild_id LIKE {guild_id};")[0]
 
 # Todo: Move over to aiosqlite
--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 import fnmatch
 import os
 import discord
-import sqlite3
 import database
 from discord.ext import commands
+import logging
+import sys
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+# handler = logging.FileHandler(filename='/app/data/discord.log', encoding='utf-8', mode='w')
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 DEBUG_GUILDS = os.environ.get("DEBUG_GUILDS")
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN") 
@@ -15,16 +23,20 @@ with os.scandir("/app/src/cogs") as fileList:
     for file in fileList:
         if fnmatch.fnmatch(file,"*.py"):
             if os.path.splitext(file.name)[0] in enabled:
-                # denbot.add_cog(f"{os.path.splitext(file.name)[0]}")
                 denbot.load_extension(f"cogs.{os.path.splitext(file.name)[0]}")
-                print(f"Cog: {os.path.splitext(file.name)[0]} added.")
+                logger.warning(f"Cog: {os.path.splitext(file.name)[0]} added.")
 
 @denbot.event
 async def on_ready():
-    print(f"{denbot.user} has successfully connected! (ID: {denbot.user.id})")
-    database.create_table()
+    logger.info(f"{denbot.user} has successfully connected! (ID: {denbot.user.id})")
+    # database.create_table()
     for guild in denbot.guilds:
         database.add_guild(guild.id)
+
+@denbot.event
+async def on_guild_join(guild):
+    database.add_guild(guild.id)
+
 
 
 denbot.run(DISCORD_TOKEN) 
